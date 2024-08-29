b2b59d47855b10ac73b17632c018a51bb236c9a9
Erim Caliskan <Erim.Caliskan@ruhr-uni-bochum.de>
2024-08-29 22:54:20 +0200

    type classes

---
+++ rollType.py
@@ -0,0 +1,42 @@
+SKILL = {
+    "str"|"stärke"|"strength": "Strength",
+    "ges"|"geschicklichkeit"|"dex"|"dexterity": "Dexterity",
+    "kon"|"con"|"konstitution"|"konsti": "Constitution",
+    "int"|"intelligence"|"intelligenz": "Intelligenz",
+    "wis"|"wisdom"|"weisheit"|"wei": "Wisdom",
+    "charisma"|"cha" : "Charisma",
+}
+
+ATTRIBUTE = {
+    "initiative"|"ini"|"init" : "Initiative",
+    "akrobatik"|"acrobatics" : "Acrobatics",
+    "tier"|"animal"|"animalhandling" : "Animal Handling",
+    "arkana"|"arcana" : "Arcana",
+    "athletik"|"athletics" : "Athletics",
+    "täuschen"|"deception" : "Deception",
+    "geschichte"|"history" : "Jistory",
+    "motiv"|"insight" : "Insight",
+    "einschüchtern"|"intimidation" : "Intimidation",
+    "nachforschen"|"nachforschung"|"investigation" : "Investigation",
+    "heilkunde"|"medicine" : "Medicine",
+    "naturkunde"|"nature" : "Nature",
+    "wahrnehmung"|"perception" : "Perception",
+    "auftreten"|"performance" : "Performance",
+    "überzeugen"|"persuasion" : "Persuasion",
+    "religion" : "Religion",
+    "fingerfertigkeit"|"finger"|"sleight" : "Sleight of Hand",
+    "heimlichkeit"|"stealth" : "Stealth",
+    "überleben"|"survival" : "Survival",
+}
+
+PROFICIENCY = {
+    "alchemist"|"alchemy" : "Alchemistkit",
+    "thieves"|"dieb" : "Thieves Cant",
+    "kaligraphie"|"calycraphy" : "Calygraphykit",
+    "schmied"|"smith": "Smithing Tools",
+    "koch"|"cooking" : "Cooking Tools",
+    "tinker"|"tüftler" : "Tinker Tools",
+    "navigator": "Navigator",
+    "vehicles": "Vehicles",
+    "heilkräuter"|"herbalism" : "Herbalismkit",
+}
