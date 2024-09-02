4c013d8a3b75a1d411d23745aed21892aca221e3
CourierNR6 <robinalptonguc@gmail.com>
2024-09-02 15:48:20 +0200

    more Easteregg

--- customTypes/easterEggs.py
+++ customTypes/easterEggs.py
@@ -1,4 +1,8 @@
 EASTEREGG = {
     "info" : "Hallo! ich bin der DND Bot und wurde von CourierNr6 und TheFeThrone geschrieben. Viel Spaß beim Rollen!",
     "easteregg" : "Ostern ist vorbei. Such wo anders",
+    "kasper" : "Ich hoffe du schaust nicht wieder in die Sonne",
+    "ende" : "Das ist das Ende",
+    "levelup" : "Nein",
+
 }
